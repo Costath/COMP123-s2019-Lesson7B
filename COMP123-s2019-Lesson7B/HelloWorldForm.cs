f0e6728893a9ac94060363c69e7cad14699c8935
Costath <thalesdaniel@gmail.com>
2019-06-24 14:25:14 -0400

    Added functionality for my "Hello, World!" program.

---
+++ COMP123-s2019-Lesson7B/HelloWorldForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_s2019_Lesson7B
+{
+    public partial class HelloWorldForm : Form
+    {
+        /// <summary>
+        /// This is the HelloWorldForm constructor.
+        /// </summary>
+        public HelloWorldForm()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        /// <summary>
+        /// The start method is used for property initialization.
+        /// </summary>
+        public void Start()
+        {
+            HelloWorldLabel.BackColor = Color.Black;
+            HelloWorldLabel.ForeColor = Color.White;
+            HelloWorldLabel.Text = "Hello, Thales";
+
+            GenerateRandomNumber();
+        }
+
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            //Ternary operator
+            HelloWorldLabel.Text = (HelloWorldLabel.Text == "Clicked!") ? "Unclicked!" : "Clicked!";
+
+            GenerateRandomNumber();
+        }
+
+        /// <summary>
+        /// This methods creates a random integer from 1 to 49.
+        /// </summary>
+        private void GenerateRandomNumber()
+        {
+            RandomNumberLabel.Text = (new Random().Next(1, 49) + 1).ToString();
+        }
+    }
+}
